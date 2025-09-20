6f472861ab798b58c876bcfd868ca916a20dd803
Anoosha Khan <anoosha.devx@gmail.com>
2025-09-20 12:00:02 +0500

    Update number_guessing_game.py: move random inside loop and add exit message

--- Course1/number_guessing_game.py
+++ Course1/number_guessing_game.py
@@ -1,9 +1,9 @@
 #Number Guessing Game
 print("Welcome to Number Guessing Game!!! 🤯")
 import random
-guess = random.randint(1,20)
 print("I picked a number between 1 and 20. You have 5 attempts!")
 while True:
+	guess = random.randint(1,20)
 	for attempt in range(1,6):
 		try:
 			num = int(input(f'Attempt {attempt} - Guess a number: '))
@@ -15,6 +15,7 @@ while True:
 				print("Yayyy!!! You got it correct 🎯🔥")
 				print(f"The correct number is: {guess}")
 				print(f"Your attempts: {attempt}")
+				print("Thanks for playing!💗)
 				break
 		except ValueError:
 			print("Please enter a number only!")
@@ -22,5 +23,5 @@ while True:
 		print(f"Out of attempts ☹️. The number was {guess}")
 	repeat = input("Do you want to play again? y/n: ")
 	if repeat.lower() != "y":
-		print("Thanks for playing! 🫶")
+		print("Thanks for playing!💗")
 		break
